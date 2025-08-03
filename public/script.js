820f95b156aa7180d1741007cd48a13d26d0d036
Eric Schmidt <eric.schmidt.2009@icloud.com>
2025-08-03 21:56:39 +0200

    Synchronisation i think

--- public/script.js
+++ public/script.js
@@ -120,4 +120,19 @@ document.addEventListener('DOMContentLoaded', () => {
       authCard.classList.remove('slide-register');
     });
   }
+
+  const container = document.getElementById('container');
+  const registerBtn = document.getElementById('register');
+  const loginBtn = document.getElementById('login');
+
+  if (registerBtn && container) {
+    registerBtn.addEventListener('click', () => {
+      container.classList.add("active-panel");
+    });
+  }
+  if (loginBtn && container) {
+    loginBtn.addEventListener('click', () => {
+      container.classList.remove("active-panel");
+    });
+  }
 });
